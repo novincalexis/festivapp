d8b7b1ff15b82ba592628e9668c42e82817639ab
OUAIS MON FRERE <jeralexis77@aol.com>
2022-07-25 23:17:12 +0200

    db fix again

--- festivapp/config/database.php
+++ festivapp/config/database.php
@@ -68,7 +68,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'festivapp'),
+            'database' => env('DB_DATABASE', 'd3pefbmdfddetm'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
