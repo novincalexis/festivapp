14e87e57b708d4953e41618846be7909d9536d97
OUAIS MON FRERE <jeralexis77@aol.com>
2022-08-11 18:51:18 +0200

    fix dashboard conusult

--- festivapp/resources/js/pages/Dashboard.vue
+++ festivapp/resources/js/pages/Dashboard.vue
@@ -1,27 +1,68 @@
 <template>
-    <div>
-        Bonjour {{ name }}
+
+<div class="card">
+    <div class="card-body">
+        <div class="d-flex justify-content-between pb-2 mb-2">
+            <h5 class="card-title">All Posts Data</h5>
+        </div>
+        
+        <table class="table table-hover table-sm">
+            <thead class="bg-dark text-light">
+                <tr>
+                    <th width="50" class="text-center">#</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th class="text-center" width="120">Image</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(post, index) in posts" :key="post.id">
+                    <td class="text-center">{{index+1}}.</td>
+                    <td>{{post.name}}</td>
+                    <td>{{post.description}}</td>
+                    <td class="text-center">
+                        <div v-if="post.image">
+                            <img alt="post-img" width="100" v-bind:src="'/img/' +post.image">
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+
     </div>
+</div>
+
+
 </template>
 
 <script>
-export default {
-    name: "Dashboard",
-    data() {
-        return {
-            name: null,
+    export default {
+        name: "Dashboard",
+        data() {
+            return {
+                posts: []
+            }
+        },
+        created() {
+            this.$axios.get('/sanctum/csrf-cookie').then(response => {
+                this.$axios.get('/api/posts')
+                .then(response => {
+                    this.posts = response.data;
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+            });
+        },
+        methods: {
+        },
+        beforeRouteEnter(to, from, next) {
+            if (!window.Laravel.isLoggedin) {
+                window.location.href = "/";
+            }
+            next();
         }
-    },
-    created() {
-        if (window.Laravel.user) {
-            this.name = window.Laravel.user.name
-        }
-    },
-    beforeRouteEnter(to, from, next) {
-        if (!window.Laravel.isLoggedin) {
-            window.location.href = "/";
-        }
-        next();
     }
-}
+
 </script>
