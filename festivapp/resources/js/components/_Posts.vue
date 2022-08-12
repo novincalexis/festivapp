52747389d3e1458ae3c4dc3a39f3bbf60e062fca
OUAIS MON FRERE <jeralexis77@aol.com>
2022-08-12 13:59:00 +0200

    page full post

--- festivapp/resources/js/components/_Posts.vue
+++ festivapp/resources/js/components/_Posts.vue
@@ -1,16 +1,18 @@
 <template>
 
 <div class="festlist">
-    <p id="href" v-text="this.$route.query.page" />
-    <div class="cards">
-        <div v-for="(post) in posts" :key="post.id" class="card">
-            <div v-if="post.image" class="card-img">
-                <img alt="post-img" width="100" v-bind:src="'/img/' +post.image">
-            </div>
-            <div class="card-detail">
-                <p class="name">{{post.name}}</p>
-                <p class="description">{{post.description}}</p>
-                <p class="date">Posté le {{post.created_at}}</p>
+    <p id="href" v-text="this.$route.query.page"></p>
+    <div class="cards full">
+        <div style="width: calc(100% - 50px);" v-for="(post) in posts" :key="post.id">
+            <div style="width:100%" v-if="post.id == postId">
+                <div v-if="post.image" class="card-img">
+                    <img alt="post-img" width="100" v-bind:src="'/img/' +post.image">
+                </div>
+                <div class="card-detail">
+                    <p class="name">{{post.name}}</p>
+                    <p class="description">{{post.description}}</p>
+                    <p class="date">Posté le {{post.created_at}}</p>
+                </div>
             </div>
         </div>
     </div>
@@ -24,24 +26,28 @@
         name: "PostsDetail",
         data() {
             return {
-                posts: []
+                posts: [],
+                postId,
+                pathArray
             }
         },
         created() {
             this.$axios.get('/sanctum/csrf-cookie').then(response => {
                 this.$axios.get('/api/posts')
                 .then(response => {
+                    /*this.pathArray = window.location.pathname.split('/');
+                    this.postId = pathArray[2];
+                    console.log(postId);*/
                     this.posts = response.data;
                 })
                 .catch(function(error) {
                     console.log(error);
                 });
             });
-        },
-        mounted() {
-            console.log(window.location.pathname)
         }
     }
-            let url = window.location.pathname
+
+    let pathArray = window.location.pathname.split('/');
+    var postId = pathArray[2];
 
 </script>
